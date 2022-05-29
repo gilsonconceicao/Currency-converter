5f53bc1529ad7f8be6be56279546589548225852
Junior Santos <gilsonsantosjunior02@gmail.com>
2022-05-29 15:36:51 -0300

    changes of JS

--- converterMoney.js
+++ converterMoney.js
@@ -1,57 +1,60 @@
 const btn = document.getElementById('converter-value')
 const optionCurrency = document.getElementById('changes-currency')
 
+// prefixed changes 
 const dolar = 4.73
 const euro = 5.08
 
+
 const converterMoney = () => {
+    
+
+    const addValue = document.getElementById('add-value').value
+    if (addValue == '' || addValue == 0) {
+        alert('Adicione um valor válido!')
+    } else {
+        changesCurrency()
+        document.querySelector('.area-response').style.display = 'flex'
+    }
+
+    
+}
+
+function changesCurrency() {
+    // a DOM
     const addValue = document.getElementById('add-value').value
     const currencyBRL = document.getElementById('currencyBRL')
     const currencyTransform = document.getElementById('currency-transform')
 
+
     currencyBRL.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: 'currency', 
-        currency: 'BRL', 
+        style: 'currency',
+        currency: 'BRL',
     }).format(addValue)
 
-    currencyTransform.innerHTML = new Intl.NumberFormat("en-US", {
-        style: 'currency', 
-        currency: 'USD', 
-    }).format(addValue / dolar)
+    // option select converter 
+    const optionCurrency = document.getElementById('changes-currency')
 
-}
+    if (optionCurrency.value === 'Dolar') {
 
-changesCurrency = () => {
-    const currency = document.getElementById('changes-currency') 
-    console.log(currency.value)
+        currencyTransform.innerHTML = new Intl.NumberFormat("en-US", {
+            style: 'currency',
+            currency: 'USD',
+        }).format(addValue / dolar)
 
-    if (currency.value === 'Dolar') {
-        converterMoney()
+        document.querySelector('.image-change').src = 'assets/estados-unidos.png'
+        document.querySelector('.changeNameCurrency').innerHTML = 'Dólar'
 
-    } else if (currency.value === 'Euro') {
-        const addValue = document.getElementById('add-value').value
-        const currencyBRL = document.getElementById('currencyBRL')
-        const currencyTransform = document.getElementById('currency-transform')
-
-        // change image 
-        document.getElementsByClassName('image-change')[0].src = 'assets/uniao-europeia.png' 
-        
-        // euro currency calculation
-        
-        currencyBRL.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: 'currency', 
-            currency: 'BRL', 
-        }).format(addValue)
-    
+    } else {
         currencyTransform.innerHTML = new Intl.NumberFormat("de-DE", {
             style: 'currency', 
             currency: 'EUR', 
         }).format(addValue / euro)
 
-        document.getElementsByClassName('changeNameCurrency')[0].innerHTML = 'Euro'
+        document.querySelector('.image-change').src = 'assets/uniao-europeia.png'
+        document.querySelector('.changeNameCurrency').innerHTML = 'Euro'
     }
-
 }
 
-btn.addEventListener('click', converterMoney) 
+btn.addEventListener('click', converterMoney)
 optionCurrency.addEventListener('change', changesCurrency)
